14725f548fd0fe1ca0f473691b31625599de8bb2
Victor Barbaros <barbarosvictor@yahoo.com>
2018-08-28 20:41:50 -0400

    fixing imports on remote

--- crimeapp/__init__.py
+++ crimeapp/__init__.py
@@ -6,7 +6,7 @@ from flask_bcrypt import Bcrypt
 # Initialize application
 app = Flask(__name__, static_folder=None)
 
-from crimeapp import dbconfig
+#from crimeapp import dbconfig
 # Import the application views
 from crimeapp import crimemap
 

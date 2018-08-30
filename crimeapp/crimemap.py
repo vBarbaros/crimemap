1227dcfe351e3aafe12c273ed0aeb1ed3a7c8a38
Victor Barbaros <barbarosvictor@yahoo.com>
2018-08-29 20:11:36 -0400

    Added mockDB and refined google-map widget;

--- crimeapp/crimemap.py
+++ crimeapp/crimemap.py
@@ -1,19 +1,27 @@
 from crimeapp import app
-from dbhelper import DBHelper
+
 from flask import Flask
 from flask import render_template
 from flask import request
+
+import json
+import dbconfig
+if dbconfig.test:
+    from mockdbhelper import MockDBHelper as DBHelper
+else:
+    from dbhelper import DBHelper
 
 DB = DBHelper()
 
 @app.route("/")
 def home():
     try:
-        data = DB.get_all_inputs()
+        crimes = DB.get_all_crimes()
+        crimes = json.dumps(crimes)
     except Exception as e:
         print e
         data = None
-    return render_template("home.html", data=data)
+    return render_template("home.html", crimes=crimes)
    
 @app.route("/add", methods=["POST"])
 def add():
@@ -32,6 +40,13 @@ def clear():
         print e
     return home()
    
+@app.route("/submitcrime", methods=['POST'])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description")
+    DB.add_crime(category, date, latitude, longitude, description)
+    return home()
 
-# if __name__ == '__main__':
-#     app.run(port=5000, debug=True)
